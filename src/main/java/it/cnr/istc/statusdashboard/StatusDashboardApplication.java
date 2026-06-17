07e9c8494860fdb7326a66ab3eeb30bf7e4cc96b
Dominik <dominikkovacs16@gmail.com>
2026-06-17 11:20:21 +0200

    first-commit

---
+++ src/main/java/it/cnr/istc/statusdashboard/StatusDashboardApplication.java
@@ -0,0 +1,13 @@
+package it.cnr.istc.statusdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/cnr/istc/statusdashboard/StatusDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cnr.istc.statusdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
